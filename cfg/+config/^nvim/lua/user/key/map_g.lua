bf7bda12646a0c18ac19dcc78c3a5570b4a3641a
andyl <andy@r210.com>
2022-10-25 09:30:32 -0700

    2210250930

--- cfg/+config/^nvim/lua/user/key/map_g.lua
+++ cfg/+config/^nvim/lua/user/key/map_g.lua
@@ -3,6 +3,7 @@
 -- reference
 -- https://sbulav.github.io/vim/neovim-opening-urls/
 
+-- this is for opening URLs in the browser
 -- to use: in normal mode, put cursor over URL, then type 'gx'
 -- also note: gf jumps to file under cursor
 
