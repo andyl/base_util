70c5db074bf2a65fc41983db401ed746095350da
andyl <andy@r210.com>
2023-03-07 16:59:30 -0800

    2303071659

--- cfg/+config/^nvim/lua/user/lsp/mason.lua
+++ cfg/+config/^nvim/lua/user/lsp/mason.lua
@@ -14,11 +14,18 @@
 -- Mason log | ~/.local/state/nvim/mason.log
 -- LSP log   | ~/.local/state/nvim/lsp.log
 
+-- MASON USES LSPCONFIG:
+-- https://github.com/neovim/nvim-lspconfig
+-- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
+--
+-- LSPCONFIG HELP:
+-- :help lsp-config
+--
+-- EXAMPLE LSP config:
+-- https://github.com/megalithic/dotfiles/blob/94cce5036d8fc19c74b7a259f084560246194fe5/config/nvim/lua/mega/plugins/lsp/servers.lua#L477-L546
 -- https://github.com/williamboman/mason.nvim
 -- https://github.com/williamboman/mason-lspconfig.nvim
--- https://github.com/neovim/nvim-lspconfig
-
--- start mason
+--
 
 require'mason'.setup()
 
@@ -29,11 +36,12 @@ local servers = {
   "cssls",       -- css
   "dockerls",    -- dockerfile
   "elixirls",    -- elixir
+  "emmet_ls",    -- emmet
   "html",        -- html
   "jsonls",      -- json
   "marksman",    -- markdown
   "solargraph",  -- ruby
-  -- "sumneko_lua", -- lua
+  "sqlls",       -- sql
   "tailwindcss", -- tailwind
   "tsserver",    -- javascript and typescript
   "yamlls",      -- yaml
@@ -55,9 +63,6 @@ require'mason-lspconfig'.setup_handlers {
   function (server_name) -- default handler sets up all servers
     require"lspconfig"[server_name].setup {}
   end,
-  -- ["sumneko_lua"] = function ()      -- custom settings for LUA
-  --   server_config("sumneko_lua")
-  -- end,
   ["jsonls"] = function ()           -- custom settings for JSON
     server_config("jsonls")
   end,
--- cfg/+config/^nvim/lua/user/lsp/settings/jsonls.lua
+++ cfg/+config/^nvim/lua/user/lsp/settings/jsonls.lua
@@ -1,8 +1,4 @@
-local default_schemas = nil
 local status_ok, jsonls_settings = pcall(require, "nlspsettings.jsonls")
-if status_ok then
-  default_schemas = jsonls_settings.get_default_schemas()
-end
 
 local schemas = {
   {
