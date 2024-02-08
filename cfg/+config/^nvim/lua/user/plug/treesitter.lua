3c5ce003e31d6e32cf2513f13060f3b733eb13ed
andyl <andy@r210.com>
2024-02-08 07:13:46 -0800

    Reconfigure blocks and functions

--- cfg/+config/^nvim/lua/user/plug/treesitter.lua
+++ cfg/+config/^nvim/lua/user/plug/treesitter.lua
@@ -54,32 +54,36 @@ require'nvim-treesitter.configs'.setup{
       lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
       keymaps = {
         -- You can use the capture groups defined in textobjects.scm
-        ['aa'] = '@parameter.outer',
-        ['ia'] = '@parameter.inner',
-        ['af'] = '@function.outer',
-        ['if'] = '@function.inner',
         ['ac'] = '@class.outer',
         ['ic'] = '@class.inner',
+        ['af'] = '@function.outer',
+        ['if'] = '@function.inner',
+        ['ab'] = '@block.outer',
+        ['ib'] = '@block.inner',
       },
     },
     move = {
       enable = true,
       set_jumps = true, -- whether to set jumps in the jumplist
       goto_next_start = {
-        [']m'] = '@function.outer',
-        [']]'] = '@class.outer',
+        [']]'] = '@function.outer',
+        [']f'] = '@function.outer',
+        [']b'] = '@block.outer',
       },
       goto_next_end = {
-        [']M'] = '@function.outer',
-        [']['] = '@class.outer',
+        [']['] = '@function.outer',
+        [']F'] = '@function.outer',
+        [']B'] = '@block.outer',
       },
       goto_previous_start = {
-        ['[m'] = '@function.outer',
-        ['[['] = '@class.outer',
+        ['[['] = '@function.outer',
+        ['[f'] = '@function.outer',
+        ['[b'] = '@block.outer',
       },
       goto_previous_end = {
-        ['[M'] = '@function.outer',
-        ['[]'] = '@class.outer',
+        ['[]'] = '@function.outer',
+        ['[F'] = '@function.outer',
+        ['[B'] = '@block.outer',
       },
     },
   },
