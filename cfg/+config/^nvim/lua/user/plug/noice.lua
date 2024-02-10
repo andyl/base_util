600e5106388e737d511df5f6d156630583c61b6f
andyl <andy@r210.com>
2024-02-10 11:42:36 -0800

    2402101142

--- cfg/+config/^nvim/lua/user/plug/noice.lua
+++ cfg/+config/^nvim/lua/user/plug/noice.lua
@@ -3,3 +3,16 @@
 require("noice").setup({})
 
 require("telescope").load_extension("noice")
+
+require("noice").setup({
+  routes = {
+    {
+      filter = {
+        event = "msg_show",
+        kind = "",
+        find = "nohl",
+      },
+      opts = { skip = true },
+    },
+  },
+})
