89fdcb1d60f25209401a5281ad8e8f906e306e0a
andyl <andy@r210.com>
2024-07-10 17:14:07 -0700

    Add map_dash

---
+++ cfg/+config/^nvim/lua/user/key/map_dash.lua
@@ -0,0 +1,31 @@
+-- key/map_dash
+
+local n_opts = {
+  mode    = "n",     -- NORMAL mode
+  prefix  = "-",    -- dash key
+  buffer  = nil,     -- Give a buffer num for buffer local mapping
+  silent  = true,    -- use `silent` when creating keymaps
+  noremap = true,    -- use `noremap` when creating keymaps
+  nowait  = true,    -- use `nowait` when creating keymaps
+}
+
+local n_keymap = {
+}
+
+if WhichKeyOk then WhichKey.register(n_keymap, n_opts) end
+
+local v_opts = {
+  mode    = "v",     -- VISUAL mode
+  prefix  = "-",     -- dash key
+  buffer  = nil,     -- Give a buffer num for buffer local mapping
+  silent  = true,    -- use `silent` when creating keymaps
+  noremap = true,    -- use `noremap` when creating keymaps
+  nowait  = true,    -- use `nowait` when creating keymaps
+}
+
+local v_keymap = {
+  }
+
+
+if WhichKeyOk then WhichKey.register(v_keymap, v_opts) end
+
