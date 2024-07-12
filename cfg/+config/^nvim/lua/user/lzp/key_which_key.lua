6f5302189f1796f3e761bb72fe46df2ac0470701
andyl <andy@r210.com>
2024-07-12 12:33:57 -0700

    Add documentation

--- cfg/+config/^nvim/lua/user/lzp/key_which_key.lua
+++ cfg/+config/^nvim/lua/user/lzp/key_which_key.lua
@@ -1,4 +1,10 @@
 -- key_which_key
 --
+-- Key bindings with auto-introspection.
+--
+-- https://github.com/folke/which-key.nvim
+-- :help which-key.nvim.txt
+--
+-- More info `lua/user/key/README.md`
 
 return 'folke/which-key.nvim'
--- cfg/+config/^nvim/lua/user/lzp/md_bullets.lua
+++ cfg/+config/^nvim/lua/user/lzp/md_bullets.lua
@@ -1,4 +1,6 @@
 -- md_bullets
 --
+-- https://github.com/bullets-vim/bullets.vim
+-- :help bullets
 
 return 'dkarter/bullets.vim'
