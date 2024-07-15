d38353cd73fd1b6c1a9a321e7804589271eba353
andyl <andy@r210.com>
2024-07-14 18:13:15 -0700

    240714-1813

--- cfg/+config/^nvim/lua/user/plug/cmp.lua
+++ cfg/+config/^nvim/lua/user/plug/cmp.lua
@@ -2,16 +2,10 @@
 --------------------------------------------------------
 
 local cmp_status_ok, cmp = pcall(require, "cmp")
-if not cmp_status_ok then
-  return
-end
+if not cmp_status_ok then return end
 
 local snip_status_ok, luasnip = pcall(require, "luasnip")
-if not snip_status_ok then
-  return
-end
-
--- require("luasnip/loaders/from_vscode").lazy_load()
+if not snip_status_ok then return end
 
 local check_backspace = function()
   local col = vim.fn.col "." - 1
